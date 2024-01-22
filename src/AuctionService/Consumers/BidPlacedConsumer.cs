0fbea682dc6bd84ac10aa0b574fa5c6e308b43e9
Milos Novakovic <milos.novakovic.1337@gmail.com>
2024-01-22 16:04:26 +0100

    End of Section 6

---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,30 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public BidPlacedConsumer(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed");
+
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
+
+        if (auction.CurrentHighestBid == null
+            || context.Message.BidStatus.Contains("Accepted")
+            && context.Message.Amount > auction.CurrentHighestBid)
+        {
+            auction.CurrentHighestBid = context.Message.Amount;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed");
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (context.Message.BidStatus.Contains("Accepted") 
+            && context.Message.Amount > auction.CurrentHighestBid)
+        {
+            auction.CurrentHighestBid = context.Message.Amount;
+            await auction.SaveAsync();
+        }
+    }
+}
